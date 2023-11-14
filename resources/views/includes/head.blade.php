79c4027568a19ae34ac212d69bbbc95310ef9c4c
Nosipho Khumalo <121792437+NosiphoSK1208@users.noreply.github.com>
2023-11-14 07:15:19 -0500

    Update FrontEnd
    I've manage to do some changes on the frontend. Improving

---
+++ resources/views/includes/head.blade.php
@@ -0,0 +1,14 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<meta name="description" content="">
+<meta name="author" content="">
+
+<title>Chromeco</title>
+
+<!-- CSS FILES -->
+<link href="{{ asset('frontend/css/bootstrap.min.css') }}" rel="stylesheet">
+
+<link href="{{ asset('frontend/css/bootstrap-icons.css') }}" rel="stylesheet">
+
+<link href="{{ asset('frontend/css/templatemo-kind-heart-charity.css') }}" rel="stylesheet">
